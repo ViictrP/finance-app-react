77b17b5fd19975340107fd3520af2ed49ff1d3fc
Victor <victor.prado@capco.com>
2022-04-24 18:36:43 -0300

    login page development

--- src/components/ui/login-form/login-form.tsx
+++ src/components/ui/login-form/login-form.tsx
@@ -6,7 +6,26 @@ import React from 'react';
 
 const { Title, Text } = Typography;
 
-const LoginForm = () => {
+type LoginFormProps = {
+  onForgotUsernameClick: () => void;
+  onForgotPasswordClick: () => void;
+  onFirstAccessClick: () => void;
+  onSubmit: (data: Data) => void;
+};
+
+type Data = {
+  username: string;
+  password: string;
+  remember: boolean;
+};
+
+const LoginForm = (props: LoginFormProps) => {
+  const [form] = Form.useForm();
+  
+  function onFinish(values: any) {
+    props.onSubmit(values);
+  }
+
   return (
     <div>
       <Title level={3}>Acessar o sistema</Title>
@@ -14,7 +33,13 @@ const LoginForm = () => {
         Ao continuar você concorda com os termos e condições do sistema.
       </Text>
       <div className="divider-2x"></div>
-      <Form name="basic" initialValues={{ remember: true }} autoComplete="off">
+      <Form
+        form={form}
+        name="basic"
+        initialValues={{ remember: true }}
+        autoComplete="off"  
+        onFinish={onFinish}
+      >
         <Form.Item
           name="username"
           rules={[{ required: true, message: 'Please input your username!' }]}
@@ -36,7 +61,24 @@ const LoginForm = () => {
           />
         </Form.Item>
         <Form.Item name="remember" valuePropName="checked">
-          <Text>Esqueceu o usuário ou senha</Text>
+          <Text>
+            Esqueceu o
+            <Button
+              style={{ padding: 4 }}
+              type="link"
+              onClick={props.onForgotUsernameClick}
+            >
+              usuário
+            </Button>
+            ou
+            <Button
+              style={{ padding: 4 }}
+              type="link"
+              onClick={props.onForgotPasswordClick}
+            >
+              senha.
+            </Button>
+          </Text>
         </Form.Item>
         <Form.Item>
           <Button
@@ -50,7 +92,16 @@ const LoginForm = () => {
           </Button>
         </Form.Item>
         <Form.Item name="remember" valuePropName="checked">
-          <Text>Primeiro acessso</Text>
+          <Text>
+            Primeiro acessso
+            <Button
+              style={{ padding: 4 }}
+              type="link"
+              onClick={props.onFirstAccessClick}
+            >
+              clique aqui.
+            </Button>
+          </Text>
         </Form.Item>
       </Form>
     </div>
--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -6,9 +6,15 @@ import LoginForm from '../../components/ui/login-form/login-form';
 const LoginPage = () => {
   return (
     <Row>
+      <Col span={24}></Col>
       <Col span={24}>
-        <div className='container'>
-          <LoginForm />
+        <div className="container">
+          <LoginForm
+            onForgotUsernameClick={() => console.log('forgot my username')}
+            onForgotPasswordClick={() => console.log('forgot my password')}
+            onFirstAccessClick={() => console.log('my first access')}
+            onSubmit={(values) => console.log(values)}
+          />
         </div>
       </Col>
     </Row>
