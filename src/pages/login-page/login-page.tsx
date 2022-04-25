a7d917bff4bbe53c5763bf719d4280f030e219e0
Victor <victor.prado@capco.com>
2022-04-24 21:59:19 -0300

    LAyout breakpoints

--- src/pages/login-page/login-page.tsx
+++ src/pages/login-page/login-page.tsx
@@ -1,24 +1,24 @@
 import './style.css';
 import React from 'react';
-import {Row, Col} from 'antd';
+import { Col, Row } from 'antd';
 import LoginForm from '../../components/ui/login-form/login-form';
 
 const LoginPage = () => {
-	return (
-		<Row>
-			<Col span={24}/>
-			<Col span={24}>
-				<div className="container">
-					<LoginForm
-						onForgotUsernameClick={() => console.log('forgot my username')}
-						onForgotPasswordClick={() => console.log('forgot my password')}
-						onFirstAccessClick={() => console.log('my first access')}
-						onSubmit={(values) => console.log(values)}
-					/>
-				</div>
-			</Col>
-		</Row>
-	);
+  return (
+    <Row className='full-height' align='middle'>
+      <Col xs={24} sm={24} md={4} lg={4} className='login-image' />
+      <Col xs={24} sm={24} md={10} lg={8}>
+        <div className='container'>
+          <LoginForm
+            onForgotUsernameClick={() => console.log('forgot my username')}
+            onForgotPasswordClick={() => console.log('forgot my password')}
+            onFirstAccessClick={() => console.log('my first access')}
+            onSubmit={(values) => console.log(values)}
+          />
+        </div>
+      </Col>
+    </Row>
+  );
 };
 
 export default LoginPage;
