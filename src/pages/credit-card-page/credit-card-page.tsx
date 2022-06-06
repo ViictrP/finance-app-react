6ebc2f07ceae15b26dda02f148e77513971e8a2b
Victor <victor.prado@capco.com>
2022-06-05 23:26:31 -0300

    fixing content behind navigation bar

--- src/pages/credit-card-page/credit-card-page.tsx
+++ src/pages/credit-card-page/credit-card-page.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './style.css';
 import { CreditCardForm } from 'components';
-import { Button } from 'antd';
 
 const CreditCardPage = () => {
     return (
-        <div>
+        <div className='container'>
             <h1>Credit cards</h1>
             <CreditCardForm />
         </div>
