e823a90d3de830135126feb53b240c642d8b722c
Victor <victor.prado@capco.com>
2022-06-11 19:40:56 -0300

    replacing build for dist

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
 app.use(favicon(__dirname + '/build/favicon.ico'));
 // the __dirname is the current directory from where the script is running
 app.use(express.static(__dirname));
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'dist')));
 app.get('/ping', function (req, res) {
     return res.send('pong');
 });
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 app.listen(port);
