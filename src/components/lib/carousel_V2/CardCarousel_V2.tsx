d5527c0bc738ee2b9947da3b02bac7a1ee7704a4
Victor <vtrownz@hotmail.com>
2022-07-24 03:22:51 -0300

    CardCarousel_V2.tsx created

---
+++ src/components/lib/carousel_V2/CardCarousel_V2.tsx
@@ -0,0 +1,46 @@
+import './style.css';
+import { useEffect, useRef, useState } from 'react';
+
+interface CardCarouselItem {
+  id: string | number;
+  value: string;
+}
+
+interface CardCarouselProps {
+  items: string[];
+  onChange?: (item: string) => void;
+}
+
+const CardCarousel = ({ items, onChange }: CardCarouselProps) => {
+  const container = useRef<any>();
+  const [__items] = useState(items);
+
+  const threshold = 0.5;
+  const observer = new IntersectionObserver(entries => {
+    entries.forEach(elem => {
+      if (elem.isIntersecting) {
+        onChange && onChange(elem.target.id);
+      }
+    });
+  }, { threshold });
+
+  useEffect(() => {
+    document.querySelectorAll('.item').forEach(elem => observer.observe(elem));
+  }, [__items]);
+
+  return (
+    <div ref={container} className="carousel-v2-container">
+      {
+        items.map((item, index) => (
+          <div
+            key={index}
+            id={`card-carousel-item-${index}`}
+            className="item">{item}
+          </div>
+        ))
+      }
+    </div>
+  );
+};
+
+export default CardCarousel;
